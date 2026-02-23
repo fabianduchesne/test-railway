b57af7634cf7448516cc19763a89d6bcdc4830fd
fabian, duchesne <fabian@dcode.eu>
2026-02-23 20:15:31 +0100

    add connection to a postgres DB

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import psycopg2
+import os
 
 app = FastAPI()
+
+
+def get_connection():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 
 @app.get("/")
@@ -11,4 +17,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/db-hello")
+def db_hello():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    version = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return {"postgres_version": version[0]}
 
